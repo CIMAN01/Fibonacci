c24e5379eed4487a2e9929bf25d6354c53f19d40
Cameron Imanpour <34729011+CIMAN01@users.noreply.github.com>
2021-02-26 13:36:32 -0800

    Create Fibonacci.js

---
+++ Fibonacci.js
@@ -0,0 +1,22 @@
+
+function fibonacciGenerator(n) {
+    //
+    var arr = [];
+    //
+    if(n === 1) {
+        arr = [0];
+    }
+    else if(n === 2){
+        arr = [0, 1];
+    }
+    else {
+        arr = [0, 1];
+        for (var i = 2; i < n; i++) {
+            arr.push(arr[arr.length - 2] + arr[arr.length - 1]); // [0, 1, 1] 
+        }
+    }
+    return arr;
+}
+
+var output = fibonacciGenerator(5);
+console.log(output);
